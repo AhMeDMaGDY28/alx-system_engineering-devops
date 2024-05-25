db005c6687703ca4f0eafea626a254c7253aa3ba
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-26 00:17:35 +0300

    2024-05-26 12:17:35 AM

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This script gathers data from an API for a given employee ID
+and exports the TODO list to a CSV file.
+Usage: python3 script.py <employee_id>
+"""
+import requests
+from sys import argv
+import json
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
+        exit(1)
+
+    try:
+        id = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        user = requests.get(url + f"user/{id}").json()
+        username = user.get("username")
+        todo = requests.get(url + "todos", params={"userId": id}).json()
+        data = {
+            id: [{
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": username
+                } for task in todo
+            ]}
+        json_data = json.dumps(data)
+        with open(f"{id}.json", mode="w", newline='') as json_file:
+            json_file.write(json_data)
+
+    except Exception:
+        print("error in the body")
