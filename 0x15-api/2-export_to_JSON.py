bbeedfd0efd485d2fd7e8e8cc98802e9e0d0b019
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-26 00:44:48 +0300

    2024-05-26 12:44:49 AM

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""This script gathers data from an API for a given employee ID
-and exports the TODO list to a CSV file.
-Usage: python3 script.py <employee_id>
-"""
-import requests
-from sys import argv
+"""Returns to-do list information for a given
+employee ID and exports it to JSON."""
 import json
+import requests
+import sys
 
 if __name__ == "__main__":
-    id = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + f"user/{id}").json()
+    id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(id)).json()
     username = user.get("username")
     todo = requests.get(url + "todos", params={"userId": id}).json()
+
     data = {
-        id: [{
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": username
-        } for task in todo
-        ]}
-    json_data = json.dumps(data, indent=4)
-    with open(f"{id}.json", mode="w", newline='') as json_file:
-        json_file.write(json_data)
+        id: [
+                {
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": username
+                } for task in todo
+        ]
+    }
+
+    jsonData = json.dumps(data, indent=4)
+
+    with open(f"{id}.json", "w", newline="") as file:
+        file.write(jsonData)
