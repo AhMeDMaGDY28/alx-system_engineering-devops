8313bfe8d8782bf9cd95fdaf3074092ba4d3c4c5
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-25 20:15:23 +0300

    2024-05-25 08:15:23 PM

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,18 +9,18 @@ if len(argv) != 2:
     print("Usage: python3 script.py <employee_id>")
     exit(1)
 
+    # Make API requests and process the data here
 if __name__ == "__main__":
     employee_id = int(argv[1])
-
+    API_BASE_URL = "https://jsonplaceholder.typicode.com/"
     try:
         # employee data
-        employee_url = f"\
-        https: // jsonplaceholder.typicode.com/users/{employee_id}"
+        employee_url = API_BASE_URL + f"users/{employee_id}"
         employee_data = requests.get(employee_url).json()
         employee_name = employee_data.get('name')
 
         # the data of to do list
-        to_do_list_url = f"https://jsonplaceholder.typicode.com/todos"
+        to_do_list_url = API_BASE_URL + f"todos"
         to_do_list_data = requests.get(to_do_list_url, params={
                                        "userId": employee_id}).json()
 
@@ -33,8 +33,8 @@ if __name__ == "__main__":
             f"Employee {employee_name} is done with tasks\
                 ({num_done_tasks}/{total_tasks}):")
 
-        for task in to_do_list_data:
-            print(f"\t{task.get('title')}")
+        for task in done_tasks:
+            print(f"\t {task}")
 
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
